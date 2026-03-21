d19a542ca90f0480c8c25da509135f7a2feafd18
Sunny1919 <104007619+Sunny1919@users.noreply.github.com>
2026-03-21 09:21:32 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 ROOTFS_DIR="/home/container/arch-rootfs"
-ARCH_URL="http://mirror.rackspace.com/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.gz"
+ARCH_URL="http://mirror.rackspace.com/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.zst"
 PROOT_URL="https://github.com/proot-me/proot/releases/download/v5.3.0/proot-v5.3.0-x86-64-static"
+ZSTD_BIN="https://github.com/facebook/zstd/releases/download/v1.5.5/zstd-v1.5.5-linux-x86_64"
 
 if [ ! -e "$ROOTFS_DIR/.installed" ]; then
-    echo "--- Đang tải Arch Linux (17GB RAM) ---"
-    mkdir -p "$ROOTFS_DIR"
-    curl -L "$ARCH_URL" -o /tmp/arch.tar.gz
-    
-    echo "--- Đang giải nén hệ thống ---"
-    tar -xzf /tmp/arch.tar.gz --strip-components=1 -C "$ROOTFS_DIR"
-    
-    echo "--- Thiết lập PRoot ---"
+    rm -rf "$ROOTFS_DIR"
     mkdir -p "$ROOTFS_DIR/usr/local/bin"
     mkdir -p "$ROOTFS_DIR/etc"
+
+    curl -L "$ARCH_URL" -o /tmp/arch.tar.zst
+    curl -L "$ZSTD_BIN" -o /tmp/zstd
+    chmod +x /tmp/zstd
+    
+    /tmp/zstd -d /tmp/arch.tar.zst -o /tmp/arch.tar
+    tar -xf /tmp/arch.tar --strip-components=1 -C "$ROOTFS_DIR"
+    
     curl -L "$PROOT_URL" -o "$ROOTFS_DIR/usr/local/bin/proot"
     chmod 755 "$ROOTFS_DIR/usr/local/bin/proot"
     
     printf "nameserver 1.1.1.1\nnameserver 8.8.8.8" > "$ROOTFS_DIR/etc/resolv.conf"
+    
+    rm -rf /tmp/arch.tar.zst /tmp/arch.tar /tmp/zstd
     touch "$ROOTFS_DIR/.installed"
 fi
 
-echo "--- ĐANG VÀO ARCH LINUX ---"
 "$ROOTFS_DIR/usr/local/bin/proot" \
 --rootfs="$ROOTFS_DIR" \
 --link2symlink \

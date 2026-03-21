38a670f0b0e2e96d017b3f2f5b03ff6592f82218
Sunny1919 <104007619+Sunny1919@users.noreply.github.com>
2026-03-21 09:12:51 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 ROOTFS_DIR="/home/container/arch-rootfs"
-ARCH_URL="https://mirror.rackspace.com/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.gz"
+ARCH_URL="http://mirror.rackspace.com/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.gz"
 PROOT_URL="https://github.com/proot-me/proot/releases/download/v5.3.0/proot-v5.3.0-x86-64-static"
 
 if [ ! -e "$ROOTFS_DIR/.installed" ]; then
-    curl -Lo /tmp/arch.tar.gz "$ARCH_URL"
+    echo "--- Đang tải Arch Linux (17GB RAM) ---"
     mkdir -p "$ROOTFS_DIR"
+    curl -L "$ARCH_URL" -o /tmp/arch.tar.gz
+    
+    echo "--- Đang giải nén hệ thống ---"
     tar -xzf /tmp/arch.tar.gz --strip-components=1 -C "$ROOTFS_DIR"
     
+    echo "--- Thiết lập PRoot ---"
     mkdir -p "$ROOTFS_DIR/usr/local/bin"
-    curl -Lo "$ROOTFS_DIR/usr/local/bin/proot" "$PROOT_URL"
+    mkdir -p "$ROOTFS_DIR/etc"
+    curl -L "$PROOT_URL" -o "$ROOTFS_DIR/usr/local/bin/proot"
     chmod 755 "$ROOTFS_DIR/usr/local/bin/proot"
     
-    echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" > "$ROOTFS_DIR/etc/resolv.conf"
-    
+    printf "nameserver 1.1.1.1\nnameserver 8.8.8.8" > "$ROOTFS_DIR/etc/resolv.conf"
     touch "$ROOTFS_DIR/.installed"
 fi
 
+echo "--- ĐANG VÀO ARCH LINUX ---"
 "$ROOTFS_DIR/usr/local/bin/proot" \
 --rootfs="$ROOTFS_DIR" \
 --link2symlink \
